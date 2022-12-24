5075667917da0b445c16d04de6368ebc823b33cd
hapsoa <hapsoa@gmail.com>
2022-12-25 01:18:29 +0900

    버튼에 timer를 관리하는 기능을 추가함.

--- app/src/main/java/com/example/androidpractice/MainActivity.kt
+++ app/src/main/java/com/example/androidpractice/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import java.util.*
+import kotlin.concurrent.timer
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,11 +13,25 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val tv: TextView = findViewById(R.id.tv_hello)
-
         val btn: Button = findViewById(R.id.btn_kor)
+
+        var sec: Int = 0
+        var timerTask: Timer? = null
+        var isRunning: Boolean = false
+
         btn.setOnClickListener {
-            tv.text = "안녕"
+            isRunning = !isRunning
+
+            if (isRunning) {
+                timerTask = timer(period = 1000) {
+                    sec++
+                    runOnUiThread {
+                        tv.text = sec.toString()
+                    }
+                }
+            } else {
+                timerTask?.cancel()
+           }
         }
-        
     }
 }

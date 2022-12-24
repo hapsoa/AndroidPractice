438f1b0d8173fdf6cc24cbc2417b97a92ff7cf07
hapsoa <hapsoa@gmail.com>
2022-12-25 00:52:40 +0900

    버튼 누를시 텍스트가 변경되도록 함.

--- app/src/main/java/com/example/androidpractice/MainActivity.kt
+++ app/src/main/java/com/example/androidpractice/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.androidpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tv: TextView = findViewById(R.id.tv_hello)
+
+        val btn: Button = findViewById(R.id.btn_kor)
+        btn.setOnClickListener {
+            tv.text = "안녕"
+        }
+        
     }
 }

1adbe257bd94154df1e2c2a7d287e45d3df5a9c4
hapsoa <hapsoa@gmail.com>
2022-12-25 01:34:24 +0900

    목표 시간에 근접할 때 버튼 클릭하는 게임을 추가함.

--- app/src/main/java/com/example/androidpractice/MainActivity.kt
+++ app/src/main/java/com/example/androidpractice/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import java.lang.Math.abs
 import java.util.*
 import kotlin.concurrent.timer
 
@@ -12,25 +13,35 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val tv: TextView = findViewById(R.id.tv_hello)
-        val btn: Button = findViewById(R.id.btn_kor)
+        val tv: TextView = findViewById(R.id.tv_random)
+        val tv_t: TextView = findViewById(R.id.tv_timer)
+        val tv_p: TextView = findViewById(R.id.tv_point)
+        val btn: Button = findViewById(R.id.btn_main)
 
         var sec: Int = 0
         var timerTask: Timer? = null
         var isRunning: Boolean = false
 
+
+        val random_box = Random()
+        val num = random_box.nextInt(1001)
+        tv.text = (num.toFloat() / 100).toString()
+
+
         btn.setOnClickListener {
             isRunning = !isRunning
 
             if (isRunning) {
-                timerTask = timer(period = 1000) {
+                timerTask = timer(period = 10) {
                     sec++
                     runOnUiThread {
-                        tv.text = sec.toString()
+                        tv_t.text = (sec.toFloat()/100).toString()
                     }
                 }
             } else {
                 timerTask?.cancel()
+                val point = abs(sec - num).toFloat() / 100
+                tv_p.text = point.toString()
            }
         }
     }
